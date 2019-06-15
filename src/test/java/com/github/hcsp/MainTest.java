a140f0f6d308c7064c7819c158e72299aeaf4a1d
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:07 +0800

    Update repo

--- src/test/java/com/github/hcsp/MainTest.java
+++ src/test/java/com/github/hcsp/MainTest.java
@@ -1,19 +1,17 @@
 package com.github.hcsp;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
     @Test
     public void linkedListTest() {
         LinkedListNode l = Main.createDoublyLinkedList();
-        assertEquals(1, l.value);
-        assertEquals(2, l.next.value);
-        assertEquals(3, l.next.next.value);
-        assertEquals(null, l.next.next.next);
-        assertTrue(l.next.prev == l);
-        assertTrue(l.next.next.prev == l.next);
+        Assertions.assertEquals(1, l.value);
+        Assertions.assertEquals(2, l.next.value);
+        Assertions.assertEquals(3, l.next.next.value);
+        Assertions.assertEquals(null, l.next.next.next);
+        Assertions.assertTrue(l.next.prev == l);
+        Assertions.assertTrue(l.next.next.prev == l.next);
     }
 }
