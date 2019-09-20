48b89a0967136a682599a44d7e4d59d756af66d2
Gnosis <gaojing1996@vip.qq.com>
2019-09-20 15:00:29 +0800

    create the doubly linked list. (#1)
    create the doubly linked list. (#1)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -11,5 +11,21 @@ public class Main {
 
     // Create a doubly linked list: 1 ⇋ 2 ⇋ 3 and return the head node
     // 创建一个这样的双向链表：1 ⇋ 2 ⇋ 3 并返回头节点
-    public static LinkedListNode createDoublyLinkedList() {}
+    public static LinkedListNode createDoublyLinkedList() {
+        LinkedListNode first = new LinkedListNode();
+        first.value = 1;
+
+        LinkedListNode second = new LinkedListNode();
+        second.value = 2;
+
+        LinkedListNode third = new LinkedListNode();
+        third.value = 3;
+
+        first.next = second;
+        second.prev = first;
+        second.next = third;
+        third.prev = second;
+
+        return first;
+    }
 }
