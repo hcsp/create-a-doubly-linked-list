2ccdde87b766aff58651b0c4cd40faaa033dc15a
Yuanmeng Xia <xymreasonxxs@163.com>
2019-10-25 23:25:33 +0800

    linkedlist node (#8)
    linkedlist node (#8)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -11,5 +11,18 @@ public class Main {
 
     // Create a doubly linked list: 1 ⇋ 2 ⇋ 3 and return the head node
     // 创建一个这样的双向链表：1 ⇋ 2 ⇋ 3 并返回头节点
-    public static LinkedListNode createDoublyLinkedList() {}
+    public static LinkedListNode createDoublyLinkedList() {
+        LinkedListNode head = new LinkedListNode();
+        head.value = 1;
+        LinkedListNode middle = new LinkedListNode();
+        middle.value = 2;
+        LinkedListNode tail = new LinkedListNode();
+        tail.value = 3;
+        head.next = middle;
+        middle.prev = head;
+        middle.next = tail;
+        tail.prev = middle;
+        return head;
+    }
+
 }
