ea028860c6717d489e627126f36bb23718644b92
NervousOrange <zch693922442@outlook.com>
2020-01-26 15:10:23 +0100

    from NervousOrange: Assignment 1 of data structure.

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -11,5 +11,27 @@ public class Main {
 
     // Create a doubly linked list: 1 ⇋ 2 ⇋ 3 and return the head node
     // 创建一个这样的双向链表：1 ⇋ 2 ⇋ 3 并返回头节点
-    public static LinkedListNode createDoublyLinkedList() {}
+    public static LinkedListNode createDoublyLinkedList() {
+        LinkedListNode firstNode = createFirstDoublyLinkedList(1);
+        addLast(2);
+        addLast(3);
+        return firstNode;
+    }
+    static LinkedListNode firstNode;
+    private static LinkedListNode createFirstDoublyLinkedList(int value) {
+        firstNode = new LinkedListNode();
+        firstNode.value = value;
+        firstNode.prev = firstNode;               // 头节点的prev永远指向最后一个节点。
+        firstNode.next = null;
+        return firstNode;
+    }
+
+    private static void addLast(int value) {
+        LinkedListNode newLastLinkedListNode = new LinkedListNode();
+        newLastLinkedListNode.value = value;
+        newLastLinkedListNode.prev = firstNode.prev;
+        newLastLinkedListNode.next = null;
+        firstNode.prev.next = newLastLinkedListNode;     // 原本的最后一个节点的next 指向新的最后一个节点。
+        firstNode.prev = newLastLinkedListNode;         // 更新头节点.prev 到最后一个节点。
+    }
 }
