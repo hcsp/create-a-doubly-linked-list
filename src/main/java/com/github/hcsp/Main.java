632555a578b471ecaaf16c55f2a068367f135994
ws1603 <18388866683@163.com>
2020-02-17 21:27:56 +0800

    创建双向链表

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -11,5 +11,15 @@ public class Main {
 
     // Create a doubly linked list: 1 ⇋ 2 ⇋ 3 and return the head node
     // 创建一个这样的双向链表：1 ⇋ 2 ⇋ 3 并返回头节点
-    public static LinkedListNode createDoublyLinkedList() {}
+    public static LinkedListNode createDoublyLinkedList() {
+        LinkedListNode head = new LinkedListNode();
+        head.value = 1;
+        head.next = new LinkedListNode();
+        head.next.value = 2;
+        head.next.prev = head;
+        head.next.next = new LinkedListNode();
+        head.next.next.value = 3;
+        head.next.next.prev = head.next;
+        return head;
+    }
 }
