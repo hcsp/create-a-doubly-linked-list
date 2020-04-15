18ebf82570003c1615041f075e0a2825bf84c23f
zhangsht <z429882801@qq.com>
2020-04-15 19:06:53 +0800

    Java程序的基本结构练习：双向链表

--- src/main/java/com/github/hcsp/LinkedListNode.java
+++ src/main/java/com/github/hcsp/LinkedListNode.java
@@ -4,4 +4,8 @@ public class LinkedListNode {
     public LinkedListNode next;
     public LinkedListNode prev;
     public int value;
+
+    public LinkedListNode(int value) {
+        this.value = value;
+    }
 }
--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -11,5 +11,18 @@ public class Main {
 
     // Create a doubly linked list: 1 ⇋ 2 ⇋ 3 and return the head node
     // 创建一个这样的双向链表：1 ⇋ 2 ⇋ 3 并返回头节点
-    public static LinkedListNode createDoublyLinkedList() {}
+    public static LinkedListNode createDoublyLinkedList() {
+        LinkedListNode head = new LinkedListNode(1);
+        LinkedListNode second = new LinkedListNode(2);
+        LinkedListNode third = new LinkedListNode(3);
+
+        head.next = second;
+
+        second.prev = head;
+        second.next = third;
+
+        third.prev = second;
+
+        return head;
+    }
 }
