f773ea686aa8021b1ec274cba467a4624308b990
kongmu373 <kongmu1235@gmail.com>
2020-06-08 15:01:16 +0800

    完成双向链表

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -11,5 +11,17 @@ public class Main {
 
     // Create a doubly linked list: 1 ⇋ 2 ⇋ 3 and return the head node
     // 创建一个这样的双向链表：1 ⇋ 2 ⇋ 3 并返回头节点
-    public static LinkedListNode createDoublyLinkedList() {}
+    public static LinkedListNode createDoublyLinkedList() {
+        LinkedListNode head = new LinkedListNode();
+        LinkedListNode node = new LinkedListNode();
+        LinkedListNode tail = new LinkedListNode();
+        head.value = 1;
+        head.next = node;
+        node.value = 2;
+        node.prev = head;
+        node.next = tail;
+        tail.value = 3;
+        tail.prev = node;
+        return head;
+    }
 }
