745dc1a00cd0ead70d61a58ecbbfedd81c5c2f37
lance <491799643@qq.com>
2020-07-04 20:27:53 +0800

    建立双向链表

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,6 +1,7 @@
 package com.github.hcsp;
 
 public class Main {
+
     public static void main(String[] args) {
         // Complete createDoublyLinkedList() method to make the program output "3,2,1"
         // 补全createDoublyLinkedList()使得程序输出"3,2,1"
@@ -11,5 +12,19 @@ public class Main {
 
     // Create a doubly linked list: 1 ⇋ 2 ⇋ 3 and return the head node
     // 创建一个这样的双向链表：1 ⇋ 2 ⇋ 3 并返回头节点
-    public static LinkedListNode createDoublyLinkedList() {}
+    public static LinkedListNode createDoublyLinkedList() {
+        LinkedListNode node1 = new LinkedListNode();
+        node1.value = 1;
+        LinkedListNode node2 = new LinkedListNode();
+        node2.value = 2;
+        LinkedListNode node3 = new LinkedListNode();
+        node3.value = 3;
+
+        node1.next = node2;
+        node2.prev = node1;
+        node2.next = node3;
+        node2.prev = node2;
+
+        return node1;
+    }
 }
