9bd669b78abd44d1d1cdde264a9863eee24011b0
HBhakunamatata <59411479+HBhakunamatata@users.noreply.github.com>
2020-12-01 17:13:32 +0800

    created a doubly linked list (#86)
    created a doubly linked list (#86)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -11,5 +11,22 @@ public class Main {
 
     // Create a doubly linked list: 1 ⇋ 2 ⇋ 3 and return the head node
     // 创建一个这样的双向链表：1 ⇋ 2 ⇋ 3 并返回头节点
-    public static LinkedListNode createDoublyLinkedList() {}
+    public static LinkedListNode createDoublyLinkedList() {
+        LinkedListNode first = new LinkedListNode();
+        LinkedListNode second = new LinkedListNode();
+        LinkedListNode third = new LinkedListNode();
+
+        first.value = 1;
+        second.value = 2;
+        third.value = 3;
+
+        first.next = second;
+        second.prev = first;
+
+        second.next = third;
+        third.prev = second;
+
+        return first;
+
+    }
 }
